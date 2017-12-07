7ac11a169f56bad77a9b38a659afa26258506ca8
liaolintao <1413920792@qq.com>
2017-12-07 17:18:01 +0800

    refactor: adjus type

--- src/com/leroymerlin/commit/ChangeType.java
+++ src/com/leroymerlin/commit/ChangeType.java
@@ -7,17 +7,15 @@ package com.leroymerlin.commit;
  */
 public enum ChangeType {
 
-    FEAT("Features", "A new feature"),
+    FEATURE("Features", "A new feature"),
     FIX("Bug Fixes", "A bug fix"),
-    DOCS("Documentation", "Documentation only changes"),
+    DOC("Documentation", "Documentation only changes"),
     STYLE("Styles", "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"),
     REFACTOR("Code Refactoring", "A code change that neither fixes a bug nor adds a feature"),
-    PERF("Performance Improvements", "A code change that improves performance"),
+    PERFORMANCE("Performance Improvements", "A code change that improves performance"),
     TEST("Tests", "Adding missing tests or correcting existing tests"),
     BUILD("Builds", "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)"),
-    CI("Continuous Integrations", "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)"),
-    CHORE("Chores", "Other changes that don't modify src or test files"),
-    REVERT("Reverts", "Reverts a previous commit");
+    CHORE("Chores", "Other changes that don't modify src or test files");
 
     public final String title;
     public final String description;
